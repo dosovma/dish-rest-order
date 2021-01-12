4345c759e5d10818aab8fc64eafde66a621e6e8d
dosovma <dosovma@mail.ru>
2021-01-12 15:18:05 +0300

    init project

---
+++ src/test/java/ru/dosov/restrating/RestRatingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dosov.restrating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestRatingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
